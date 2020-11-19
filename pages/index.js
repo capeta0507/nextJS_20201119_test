c9c1188cb4f7eb6da42cf0b602c49aa45b1d263d
詹政勳 <nintendof1@gmail.com>
2020-11-19 12:04:30 +0800

    first_test

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import styled from 'styled-components'
+import styles from '../styles/Home.module.css'
+import '../styles/style.css'
+
+const Card = styled.div`
+  width: 20%;
+  border:1px solid red;
+  font-size:30px;
+  text-align: center;
+  box-shadow: 0 20px 30px rgba(0,0,0,0.2);
+  margin: 0 auto;
+`
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+        <Card>
+          card
+        </Card>
+        <h1 className='make_test'>test</h1>
+    </div>
+  )
+}
